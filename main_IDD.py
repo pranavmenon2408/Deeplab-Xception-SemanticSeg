0a6d46eba8dca64c4f95d5aa0554f35e42f21aa8
pranavmenon2408 <pranav2408dhruv@gmail.com>
2024-06-25 12:39:12 +0530

    committed

--- main_IDD.py
+++ main_IDD.py
@@ -2,13 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from sklearn.metrics import precision_recall_fscore_support
+from torchmetrics import JaccardIndex
 from tqdm import tqdm
 from model import DeepLabV3
-from utils_IDD import train_loader, DEVICE, NUM_CLASSES, NUM_EPOCHS, LR, calculate_accuracy,val_loader
+from utils_IDD import train_loader, DEVICE, NUM_CLASSES, NUM_EPOCHS, LR, calculate_accuracy, val_loader
 
 model = DeepLabV3(num_classes=NUM_CLASSES).to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LR)
+
+iou=JaccardIndex(task='multiclass',num_classes=NUM_CLASSES, average="weighted").to(DEVICE)
+
+
 
 for epoch in range(NUM_EPOCHS):
     model.train()
@@ -18,6 +24,7 @@ for epoch in range(NUM_EPOCHS):
         data, target = data.to(DEVICE), target.to(DEVICE)
         target=target.squeeze(1)
         target=target.long()
+        assert target.max().item() < NUM_CLASSES, f"Target contains an invalid class index, {target.max().item()}"
         #print(target)
         optimizer.zero_grad()
         output = model(data)
@@ -32,6 +39,11 @@ for epoch in range(NUM_EPOCHS):
     model.eval()
     val_loss = 0.0
     val_accuracy = 0.0
+    total_iou = 0.0
+    total_batches = 0
+    total_precision = 0.0
+    total_recall = 0.0
+    total_f1 = 0.0
     with torch.no_grad():
         for batch_idx, (images, masks) in enumerate(val_loader):
             images = images.to(DEVICE)
@@ -44,10 +56,28 @@ for epoch in range(NUM_EPOCHS):
             val_loss += loss.item()
             val_accuracy += calculate_accuracy(outputs, masks.squeeze(1))
 
+            iou_val=iou(torch.argmax(outputs, dim=1), masks.squeeze(1).long())
+            total_iou += iou_val.item()
+            total_batches += 1
+
+            predicted_labels = torch.argmax(outputs, dim=1).cpu().numpy()
+            true_labels = masks.squeeze(1).cpu().numpy()
+            precision, recall, f1, _ = precision_recall_fscore_support(true_labels.flatten(), predicted_labels.flatten(), average='weighted', zero_division=1)
+            
+            total_precision += precision
+            total_recall += recall
+            total_f1 += f1
+
+
     train_loss /= len(train_loader)
     val_loss /= len(val_loader)
     val_accuracy /= len(val_loader)
+    total_iou /= total_batches
+    total_precision /= total_batches
+    total_recall /= total_batches
+    total_f1 /= total_batches
     
-    print(f'Epoch [{epoch+1}/{NUM_EPOCHS}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}')
+    print(f'Epoch [{epoch+1}/{NUM_EPOCHS}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}, Mean IOU: {total_iou:.4f}, Precision: {total_precision:.4f}, Recall: {total_recall:.4f}, F1 Score: {total_f1:.4f}')
+
 torch.save(model.state_dict(), 'deeplabv3_IDD.pth')
 
--- mask.py
+++ mask.py
@@ -13,19 +13,18 @@ OUTPUT_VAL_MASK_DIR="/home/pranav/DeepLabV3_Xception/idd-20k-II/idd20kII/mask/va
 
 num_classes = 34  # Number of classes in your dataset
 
-labels={}
+labels={'road': 1, 'sky': 2, 'drivable fallback': 3, 'vehicle fallback': 4, 'non-drivable fallback': 5, 'curb': 6, 'obs-str-bar-fallback': 7, 'vegetation': 8, 'pole': 9, 'billboard': 10, 'building': 11, 'truck': 12, 'wall': 13, 'rider': 14, 'motorcycle': 15, 'autorickshaw': 16, 'car': 17, 'person': 18, 'fence': 19, 'traffic sign': 20, 'rectification border': 21, 'bicycle': 22, 'bus': 23, 'fallback background': 24, 'polegroup': 25, 'sidewalk': 26, 'bridge': 27, 'animal': 28, 'traffic light': 29, 'out of roi': 30, 'caravan': 31, 'guard rail': 32, 'rail track': 33, 'trailer': 34, 'parking': 35, 'unlabeled': 36, 'tunnel': 37, 'train': 38, 'ego vehicle': 39, 'ground': 40, 'license plate': 41}
+
+
 
 def create_mask(json_dir, mask_dir):
-    class_id = 1
+    class_id = len(labels)
     for sub_dir_1 in os.listdir(json_dir):
         for json_file in os.listdir(os.path.join(json_dir, sub_dir_1)):
             with open(os.path.join(json_dir, sub_dir_1, json_file)) as f:
                 data = json.load(f)
                 img = np.zeros((1080,1920), dtype=np.uint8)
                 for obj in data['objects']:
-                    if obj['label'] not in labels:
-                        labels[obj['label']] = class_id
-                        class_id += 1
                     
                     if 'polygon' in obj:
                         pts = np.array(obj['polygon'], np.int32)
@@ -39,12 +38,15 @@ def create_mask(json_dir, mask_dir):
                             mask = skimage.draw.polygon2mask(image_shape=(1080, 1920), polygon=flipped_rotated_pts)
 
                             # Assign label to masked pixels
-                            img[mask] = labels[obj['label']]
+                            if obj['label'] not in labels:
+                                img[mask] = labels['unlabeled']
+                            else:
+                                img[mask] = labels[obj['label']]
                     
                 print(img)
                 print(np.unique(img))
                 print(f"Saving mask {os.path.join(mask_dir, sub_dir_1, json_file.replace('.json', '.jpg'))}")
                 os.makedirs(os.path.dirname(os.path.join(mask_dir, sub_dir_1, json_file.replace('.json', '.jpg'))), exist_ok=True)
                 cv2.imwrite(os.path.join(mask_dir, sub_dir_1, json_file.replace('.json', '.jpg')), img)
-
-create_mask(INPUT_TRAIN_JSON_DIR, OUTPUT_TRAIN_MASK_DIR)
+    print(labels)
+#create_mask(INPUT_VAL_JSON_DIR, OUTPUT_VAL_MASK_DIR)
